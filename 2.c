2a4b8a6f084b2a50c843fee745d87fd9cc1f55da
Адам Тхаркахов <70707610+xZecret@users.noreply.github.com>
2020-12-18 22:51:02 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    double arrDbl[] = {0.1, 12.12, 213.214, 45.458, 234.13};
+    double arrDbl[] = {1.31243, 13.11232, 3.14, 45.458, 24.13};
     int i, n = sizeof arrDbl / sizeof(double);
     printf("Kol=%d\n", n);
     for (i = 0; i < n; i++)
